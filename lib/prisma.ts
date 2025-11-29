d96be0078c5e0b9177ef8d2f970a796085871e50
simpleHacker0893 <simplenjenga4@gmail.com>
2025-11-29 17:58:44 +0000

    addded next-env file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-
-const databaseUrl = process.env.DATABASE_URL as string;
-const adapter = new PrismaPg({ connectionString: databaseUrl });
 
 export const prisma = new PrismaClient({
-  adapter,
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 });
 
 export default prisma;
